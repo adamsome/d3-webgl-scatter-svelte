0d96756922ff9ddf29fb8ab0a42410fe5925ec07
Adam Banda <adam.banda@perficient.com>
2021-04-15 16:36:02 -0400

    Add tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script lang="ts">
-	export let name: string;
+  export let name: string
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  @layer base {
+    html {
+      @apply bg-white;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    html.dark {
+      @apply bg-black;
+    }
+  }
+
+  @layer utilities {
+    .flex-start {
+      @apply flex justify-start items-start;
+    }
+
+    .flex-center {
+      @apply flex justify-center items-center;
+    }
+  }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: {
+    enabled: !process.env.ROLLUP_WATCH,
+    layers: ['components', 'utilities'],
+    content: ['./public/index.html', './src/**/*.svelte'],
+    options: {
+      defaultExtractor: (content) => [
+        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+        ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
+      ],
+    },
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
